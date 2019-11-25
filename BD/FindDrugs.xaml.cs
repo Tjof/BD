d4279c1de35231fd6aeae7522fd9737c990c55a9
PIBOP0116001 Тимченко Дмитрий Сергеевич <4akigame@mail.ru>
2019-11-25 15:33:02 +0700

    Доделал лекарства заменители в выводе формы

--- BD/FindDrugs.xaml.cs
+++ BD/FindDrugs.xaml.cs
@@ -27,33 +27,23 @@ namespace BD
         public FindDrugs(BAZANOWEntities model, Лекарство drug, Остановки stop)
         {
 
-
-
             InitializeComponent();
             this.model = model;
             DrugName.Text = drug.Название_лекарства;
+            Substitutes.ItemsSource = drug.Лекарство2;
 
             var search = model.GetRoutes1(drug.id_лекарство, stop.id_остановки)
-                .GroupBy(r => new { r.Название, r.Название_улицы, r.Номер_дома, r.Время_начала_работы, r.Время_окончания_работы, r.Название_остановки})
+                .GroupBy(r => new { r.Название, r.Название_улицы, r.Номер_дома, r.Время_начала_работы, r.Время_окончания_работы, r.Название_остановки, r.Порядок})
                 .Select( a => 
                     new {
-                        a.Key.Название, a.Key.Название_улицы, a.Key.Номер_дома, a.Key.Время_начала_работы, a.Key.Время_окончания_работы, a.Key.Название_остановки,
-                        Маршруты = a.Select( mo => new { mo.Номер_маршрута, mo.Вид_транспорта, mo.Разница }).Distinct().ToArray(),
+                        a.Key.Название, a.Key.Название_улицы, a.Key.Номер_дома, a.Key.Время_начала_работы, a.Key.Время_окончания_работы, a.Key.Название_остановки, 
+                        Маршруты = a.Select( mo => new { mo.Номер_маршрута, mo.Вид_транспорта, mo.Разница}).Distinct().ToArray(),
                         Лекарства = a.Select( lek => new { lek.Название_формы, lek.Цена }).Distinct().ToArray()
                 })
                 .ToArray();
-
+            
             DataContext = search;
 
-        }
-
-        private void Sort_Click(object sender, RoutedEventArgs e)
-        {
-            ICollectionView dataView = CollectionViewSource.GetDefaultView(mycontrolName.ItemsSource);
-            dataView.SortDescriptions.Clear();
-            SortDescription sd = new SortDescription("Маршруты", ListSortDirection.Ascending); // маршруты.Разница и Лекарства.Цена как сортировать?
-            dataView.SortDescriptions.Add(sd);
-            dataView.Refresh();
         }
     }
 }
