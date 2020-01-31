d4e8c3ca25060a37b4c1b76578a9fa8b0b21f35a
PIBOP0116001 Тимченко Дмитрий Сергеевич <PIBOP0116001@sibupk.su>
2020-01-31 20:44:43 +0700

    Закончил отчеты полностью. Ляпота.

--- BD/GenerateReports.xaml.cs
+++ BD/GenerateReports.xaml.cs
@@ -8,9 +8,6 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace BD
 {
-    /// <summary>
-    /// Логика взаимодействия для GenerateReports.xaml
-    /// </summary>
     public partial class GenerateReports : Window
     {
         BAZANOWEntities model;
@@ -49,151 +46,122 @@ namespace BD
             workBook = excelApp.Workbooks.Add();
             workSheet = (Excel.Worksheet)workBook.Worksheets.get_Item(1);
 
-            workSheet.Range["A1"].Value = "Январь";
-            workSheet.Range["A2"].Value = "Февраль";
-            workSheet.Range["A3"].Value = "Март";
-            workSheet.Range["A4"].Value = "Апрель";
-            workSheet.Range["A5"].Value = "Май";
-            workSheet.Range["A6"].Value = "Июнь";
-            workSheet.Range["A7"].Value = "Июль";
-            workSheet.Range["A8"].Value = "Август";
-            workSheet.Range["A9"].Value = "Сентябрь";
-            workSheet.Range["A10"].Value = "Октябрь";
-            workSheet.Range["A11"].Value = "Ноябрь";
-            workSheet.Range["A12"].Value = "Декабрь";
+            workSheet.Columns[1].ColumnWidth = 14;
+            workSheet.Columns[2].ColumnWidth = 20;
+            workSheet.get_Range("A1", "A2").Font.Size = 16;
+            workSheet.get_Range("A1", "A2").Font.Name = "Time New Roman";
+            workSheet.Range["A1"].Value = "Служба 067";
+            workSheet.Range["A2"].Value = "Отчет по лекарству " + drug.Название_лекарства + " за год";
+            workSheet.Range["A4"].Value = "Месяц";
+            workSheet.Range["B4"].Value = "Количество запросов";
+
+            workSheet.Range["A5"].Value = "Январь";
+            workSheet.Range["A6"].Value = "Февраль";
+            workSheet.Range["A7"].Value = "Март";
+            workSheet.Range["A8"].Value = "Апрель";
+            workSheet.Range["A9"].Value = "Май";
+            workSheet.Range["A10"].Value = "Июнь";
+            workSheet.Range["A11"].Value = "Июль";
+            workSheet.Range["A12"].Value = "Август";
+            workSheet.Range["A13"].Value = "Сентябрь";
+            workSheet.Range["A14"].Value = "Октябрь";
+            workSheet.Range["A15"].Value = "Ноябрь";
+            workSheet.Range["A16"].Value = "Декабрь";
 
             var january = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 1);
             if (january == null)
-            {
-                workSheet.Range["B1"].Value = 0;
-            }
+                workSheet.Range["B5"].Value = 0;
             else
-            {
-                workSheet.Range["B1"].Value = january.Запросов;
-            }
+                workSheet.Range["B5"].Value = january.Запросов;
 
             var february = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 2);
             if(february == null)
-            {
-                workSheet.Range["B2"].Value = 0;
-            }
+                workSheet.Range["B6"].Value = 0;
             else
-            {
-                workSheet.Range["B2"].Value = february.Запросов;
-            }
+                workSheet.Range["B6"].Value = february.Запросов;
 
             var march = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 3);
             if(march==null)
-            {
-                workSheet.Range["B3"].Value = 0;
-            }
+                workSheet.Range["B7"].Value = 0;
             else
-            {
-                workSheet.Range["B3"].Value = march.Запросов;
-            }
+                workSheet.Range["B7"].Value = march.Запросов;
 
             var april = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 4);
             if (april == null)
-            {
-                workSheet.Range["B4"].Value = 0;
-            }
+                workSheet.Range["B8"].Value = 0;
             else
-            {
-                workSheet.Range["B4"].Value = april.Запросов;
-            }
+                workSheet.Range["B8"].Value = april.Запросов;
 
             var may = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 5);
             if (may == null)
-            {
-                workSheet.Range["B5"].Value = 0;
-            }
+                workSheet.Range["B9"].Value = 0;
             else
-            {
-                workSheet.Range["B5"].Value = may.Запросов;
-            }
+                workSheet.Range["B9"].Value = may.Запросов;
 
             var june = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 6);
             if (june == null)
-            {
-                workSheet.Range["B6"].Value = 0;
-            }
+                workSheet.Range["B10"].Value = 0;
             else
-            {
-                workSheet.Range["B6"].Value = june.Запросов;
-            }
+                workSheet.Range["B10"].Value = june.Запросов;
 
             var july = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 7);
             if (july == null)
-            {
-                workSheet.Range["B7"].Value = 0;
-            }
+                workSheet.Range["B11"].Value = 0;
             else
-            {
-                workSheet.Range["B7"].Value = july.Запросов;
-            }
+                workSheet.Range["B11"].Value = july.Запросов;
 
             var august = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 8);
             if (august == null)
-            {
-                workSheet.Range["B8"].Value = 0;
-            }
+                workSheet.Range["B12"].Value = 0;
             else
-            {
-                workSheet.Range["B8"].Value = august.Запросов;
-            }
+                workSheet.Range["B12"].Value = august.Запросов;
 
             var september = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 9);
             if (september == null)
-            {
-                workSheet.Range["B9"].Value = 0;
-            }
+                workSheet.Range["B13"].Value = 0;
             else
-            {
-                workSheet.Range["B9"].Value = september.Запросов;
-            }
+                workSheet.Range["B13"].Value = september.Запросов;
 
             var october = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 10);
             if (october == null)
-            {
-                workSheet.Range["B10"].Value = 0;
-            }
+                workSheet.Range["B14"].Value = 0;
             else
-            {
-                workSheet.Range["B10"].Value = october.Запросов;
-            }
+                workSheet.Range["B14"].Value = october.Запросов;
 
             var november = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 11);
             if (november == null)
-            {
-                workSheet.Range["B11"].Value = 0;
-            }
+                workSheet.Range["B15"].Value = 0;
             else
-            {
-                workSheet.Range["B11"].Value = november.Запросов;
-            }
+                workSheet.Range["B15"].Value = november.Запросов;
 
             var december = model.Статистика_поиска.FirstOrDefault(x => x.id_лекарство == drug.id_лекарство && x.Месяц == 12);
             if (december == null)
-            {
-                workSheet.Range["B12"].Value = 0;
-            }
+                workSheet.Range["B16"].Value = 0;
             else
-            {
-                workSheet.Range["B12"].Value = december.Запросов;
-            }
+                workSheet.Range["B16"].Value = december.Запросов;
 
-            excelApp.Charts.Add();
-            excelApp.ActiveChart.ChartType = Excel.XlChartType.xlColumnClustered;
-            excelApp.ActiveChart.HasLegend = false;
-            excelApp.ActiveChart.HasTitle = true;
-            excelApp.ActiveChart.ChartTitle.Characters.Text = "Отчет по лекарству" + " " + drug.Название_лекарства + " " + "за год";
+            Excel.Range crange;
+            Excel.ChartObjects cb = (Excel.ChartObjects)workSheet.ChartObjects(Type.Missing);
+            Excel.ChartObject cbc = (Excel.ChartObject)cb.Add(300,60,400,250);
+            Excel.Chart cp = cbc.Chart;
 
-            excelApp.ActiveChart.Axes(Excel.XlAxisType.xlCategory).HasTitle = true;
-            excelApp.ActiveChart.Axes(Excel.XlAxisType.xlCategory).AxisTitle.Characters.Text = "Месяцы";
+            crange = workSheet.get_Range("A4", "B16");
+            cp.SetSourceData(crange, System.Reflection.Missing.Value);
+            cp.ChartType = Excel.XlChartType.xlColumnClustered;
+            cp.HasLegend = false;
+            cp.HasTitle = true;
+            cp.ChartTitle.Characters.Text = "Отчет по лекарству " + drug.Название_лекарства + " за год";
+            cp.Axes(Excel.XlAxisType.xlCategory).HasTitle = true;
+            cp.Axes(Excel.XlAxisType.xlCategory).AxisTitle.Characters.Text = "Месяцы";
+
+            Excel.Borders borders = crange.Borders;
+            borders.LineStyle = Excel.XlLineStyle.xlContinuous;
 
             // Открываем созданный excel-файл
             excelApp.Visible = true;
             excelApp.UserControl = true;
+
+            this.Close();
         }
     }
 }
